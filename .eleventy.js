34f51dc732353b82f939015dedaeb7a0b643d991
George Phillips <george@cloudcannon.com>
2022-12-20 00:57:35 +0000

    Updated '.eleventy.js' via CloudCannon

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   return {
     passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "md"],
+    templateFormats: ["html", "njk", "md", "liquid"],
     dir: {
       input: "src",
       output: "_site",
